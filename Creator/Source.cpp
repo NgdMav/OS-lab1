58eb0f7eb1efd9a380b6bfc828a3f2a1082cb0bf
NgdMav <riherr00@gmail.com>
2026-02-15 10:41:26 +0300

    Creator and Reporter. Employee.h

---
+++ Creator/Source.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstring>
+
+#include "../lab1/employee.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage: Creator <filename> <record_count>\n";
+        return 1;
+    }
+
+    char* filename = argv[1];
+    int recordCount = atoi(argv[2]);
+
+    if (recordCount <= 0)
+    {
+        std::cerr << "Error: record count must be positive\n";
+        return 1;
+    }
+
+    std::ofstream file(filename, std::ios::binary);
+    if (!file)
+    {
+        std::cerr << "Error: cannot create file " << filename << "\n";
+        return 1;
+    }
+
+    std::cout << "Creator: Creating file " << filename << " with " << recordCount << " records" << "\n";
+    std::cout << "Enter employee data:\n";
+
+    employee emp;
+
+    for (int i = 0; i < recordCount; i++)
+    {
+        std::cout << "\nEmployee #" << (i + 1) << ":\n";
+
+        std::cout << "\tID number: ";
+        std::cin >> emp.num;
+
+        std::cout << "\tName (max 9 chars): ";
+        std::cin >> emp.name;
+
+        std::cout << "\tHours worked: ";
+        std::cin >> emp.hours;
+
+        file.write((char*)&emp, sizeof(employee));
+    }
+
+    file.close();
+    std::cout << "\nCreator: File created successfully!\n";
+
+    return 0;
+}
---
+++ Reporter/Source.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstring>
+
+#include "../lab1/employee.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc != 4)
+    {
+        std::cerr << "Usage: Reporter <binary_file> <report_file> <hourly_rate>\n";
+        return 1;
+    }
+
+    char* binaryFile = argv[1];
+    char* reportFile = argv[2];
+    double hourlyRate = atof(argv[3]);
+
+    if (hourlyRate <= 0)
+    {
+        std::cerr << "Error: hourly rate must be positive\n";
+        return 1;
+    }
+
+    std::ifstream binFile(binaryFile, std::ios::binary);
+    if (!binFile)
+    {
+        std::cerr << "Error: cannot open file " << binaryFile << "\n";
+        return 1;
+    }
+
+    std::ofstream repFile(reportFile);
+    if (!repFile)
+    {
+        std::cerr << "Error: cannot create report file " << reportFile << "\n";
+        binFile.close();
+        return 1;
+    }
+
+    std::cout << "Reporter: Creating report...\n";
+
+    repFile << "File report \"" << binaryFile << "\"\n";
+    repFile << std::string(70, '-') << "\n";
+    repFile << std::left << std::setw(10) << "Number"
+        << std::setw(15) << "Name"
+        << std::setw(15) << "Hours"
+        << std::setw(15) << "Salary\n";
+    repFile << std::string(70, '-') << "\n";
+
+    employee emp;
+    int count = 0;
+
+    while (binFile.read((char*)&emp, sizeof(employee)))
+    {
+        double salary = emp.hours * hourlyRate;
+
+        repFile << std::left << std::setw(10) << emp.num
+            << std::setw(15) << emp.name
+            << std::setw(15) << std::fixed << std::setprecision(2) << emp.hours
+            << std::setw(15) << std::fixed << std::setprecision(2) << salary << "\n";
+
+        count++;
+    }
+
+    repFile << std::string(70, '-') << "\n";
+    repFile << "Total employees: " << count << "\n";
+
+    binFile.close();
+    repFile.close();
+
+    std::cout << "Reporter: Report created successfully!\n";
+    std::cout << "Reporter: Total employees processed: " << count << "\n";
+
+    return 0;
+}
